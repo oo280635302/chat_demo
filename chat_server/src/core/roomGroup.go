dcd54efa609ff766c28cabc6c58b4e4f3fe44607
zy <280635302@qq.com>
2022-03-06 18:07:28 +0800

    新建房间的双读

--- chat_server/src/core/roomGroup.go
+++ chat_server/src/core/roomGroup.go
@@ -8,12 +8,17 @@ type RoomGroup struct {
 
 // 获取/新增房间信息
 func (g *RoomGroup) GetOrCreateRoom(roomId int64) *Room {
+	roomI, ok := g.Rooms.Load(roomId)
+	if ok {
+		return roomI.(*Room)
+	}
+
 	tryRoom := &Room{}
 	tryRoom.Id = roomId
 	tryRoom.Users = make(map[string]*User, 100)
 	tryRoom.HotRecord = make(map[string]int64, 100)
 
-	roomI, _ := g.Rooms.LoadOrStore(roomId, tryRoom)
+	roomI, _ = g.Rooms.LoadOrStore(roomId, tryRoom)
 	return roomI.(*Room)
 }
 
